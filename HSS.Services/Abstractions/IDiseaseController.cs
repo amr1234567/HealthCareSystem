c748485d65958cd99df18558f2ca44c036c89929
mido <loverthe480@gmail.com>
2024-12-13 10:22:19 +0200

    teba

---
+++ HSS.Services/Abstractions/IDiseaseController.cs
@@ -0,0 +1,18 @@
+﻿using DiseaseService.Dtos;
+using HSS.Domain.Enums;
+using HSS.Domain.Models;
+
+namespace DiseaseService.Abstractions
+{
+    public interface IDiseaseController
+    {
+        Task<IEnumerable<DiseaseDto>> GetAllDiseasesAsync();
+        Task<Disease> GetDiseaseAsync(int id);
+        Task<IEnumerable<Disease>> GetDiseasesAsync();
+        Task<IEnumerable<Disease>> SearchDiseasesAsync(string name, string code);
+        Task<IEnumerable<Disease>> FilterDiseasesAsync(Severity? severity, bool? contagious, int? affectedPopulation);
+        Task AddDiseaseAsync(Disease disease);
+        Task UpdateDiseaseAsync(Disease disease);
+        Task DeleteDiseaseAsync(int id);
+    }
+}

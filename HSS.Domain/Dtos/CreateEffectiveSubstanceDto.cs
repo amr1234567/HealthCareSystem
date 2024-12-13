39c9a05054341ae438b57281e48c7f72300da27c
mido <loverthe480@gmail.com>
2024-12-13 23:04:20 +0200

    Effective Substance Service

---
+++ HSS.Domain/Dtos/CreateEffectiveSubstanceDto.cs
@@ -0,0 +1,38 @@
+﻿using HSS.Domain.Models.ManyToManyRelationEntitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSS.Domain.Dtos
+{
+    public class CreateEffectiveSubstanceDto
+    {
+        
+        public string Name { get; set; }
+
+        
+        public string ChemicalFormula { get; set; }
+
+        
+        public string Description { get; set; }
+
+        
+        public DateTime DiscoveryDate { get; set; }
+
+        
+        public string ApprovedBy { get; set; }
+
+        
+        public string StabilityConditions { get; set; }
+
+        public List<SideEffectEffectiveSubstance> SideEffects { get; set; }
+
+       
+        public string PrimaryUsage { get; set; }
+
+        
+        public string AlternativeNames { get; set; }
+    }
+}
